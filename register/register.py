ed54ab8e7e968cc1f4bc590ee0c6dd966d219bee
Jelena A <94282497+jelenaart@users.noreply.github.com>
2022-01-18 20:17:24 +0200

    Update register.py

--- register/register.py
+++ register/register.py
@@ -1,13 +1,19 @@
-import module1
+from module1 import *
 users=["biba"]
 passwords=["boba"]
 valik=""
-valik=int(input("Palun valige sisselogimise viis (1-signup), (2-signin), (3-random), (4-quit)"))
-if valik==1:
-    signup()
-elif valik==2:
-    signin()
-elif valik==3:
-    random()
-elif valik==4:
-    quit
+while valik!=quit:
+    try:
+        valik=int(input("Palun valige sisselogimise viis (1-signup), (2-signin), (3-random), (4-quit)"))
+        if valik==1:
+            signup(users,passwords)
+        elif valik==2:
+            signin(users,passwords)
+        elif valik==3:
+            ran()
+        elif valik==4:
+            quit
+        else:
+            print("Midagi on valesti. Palun proovige veel kord!")
+    except ValueError:
+        print("Proovige veel kord!")
